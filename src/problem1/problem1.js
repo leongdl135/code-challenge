83fd57bc72584370f37ad69919e22527ced10fb1
leongdl135 <53109693+leongdl135@users.noreply.github.com>
2023-02-13 14:17:20 +0800

    Add Problem 1 Solution

---
+++ src/problem1/problem1.js
@@ -0,0 +1,23 @@
+var sum_to_n_a = function(n) {
+    var sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum
+};
+
+var sum_to_n_b = function(n) {
+    if (n == 0) {
+        return n
+    }
+    return n + sum_to_n_b(n-1)
+
+};
+
+var sum_to_n_c = function(n) {
+    return (n * (n + 1)) / 2
+};
+
+console.log(sum_to_n_a(2))
+console.log(sum_to_n_b(3))
+console.log(sum_to_n_b(4))
